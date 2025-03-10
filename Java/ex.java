23b155f2e01cc662c01728fed5f9e2be568844e0
Yair Klein <yairklein04@gmail.com>
2025-03-10 10:16:43 +0200

    first commit

---
+++ Java/ex.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ex {
+    public static void main(String[] args) {
+        
+    }
+    public static int maximum(int x, int y){
+        if(x > y) return x;
+        return y;
+    }
+}
