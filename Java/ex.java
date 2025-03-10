5d6d3adc9f8aca04ecd78dce258886325d7ccd65
Yair Klein <yairklein04@gmail.com>
2025-03-10 20:59:05 +0200

    changes and stuff

--- Java/ex.java
+++ Java/ex.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class ex {
     public static void main(String[] args) {
-        
+           System.out.println("hello world!");
+           Scanner in = new Scanner(System.in);
+                 
     }
     public static int maximum(int x, int y){
         if(x > y) return x;
-        return y;
+        return y;zz
     }
 }
